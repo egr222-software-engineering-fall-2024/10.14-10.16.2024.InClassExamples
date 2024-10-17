46ca52857300688cb6ed15c8c50c4bfd8a13fa6e
Larry Clement <lclement@calbaptist.edu>
2024-10-17 12:20:12 -0700

    After class on 10/17/2024

--- B/Point/src/Point.java
+++ B/Point/src/Point.java
@@ -1,1 +1,66 @@
+import java.awt.*;
+
 // TO DO:  Implement a class of our own named Point
+public class Point {
+    private int x;
+    private int y;
+
+    public static Point earthquakeOrigin;
+    public static int earthquakeRadius;
+
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void drawEarthquakeImpactArea(Graphics g) {
+        earthquakeOrigin.draw(g);
+        g.setColor(Color.RED);
+        int circleGap = 5;
+        int radius = 0;
+        while (radius <= earthquakeRadius) {
+            radius += circleGap;
+            g.drawOval(earthquakeOrigin.getX()-radius, earthquakeOrigin.getY()-radius, 2*radius, 2*radius);
+        }
+        g.setColor(Color.BLACK);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distanceTo(Point other) {
+        // the ^ is the EXCLUSIVE OR operator and NOT the exponent operator
+        int deltaX = other.getX() - x;
+        int deltaY = other.getX() - y;
+        return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+    }
+
+    public void draw(Graphics g) {
+        boolean insideEarthquakeRadius = distanceToEarthquakeOrigin() <= Point.earthquakeRadius;
+        if(insideEarthquakeRadius)
+            g.setColor(Color.RED);
+
+        g.fillOval(x, y, 3, 3);
+        g.drawString(this.toString(), x + 5, y + 5);
+
+        if(insideEarthquakeRadius)
+            g.setColor(Color.BLACK);
+    }
+
+    public double distanceToEarthquakeOrigin() {
+        return distanceTo(earthquakeOrigin);
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
--- B/Point/src/PointClient.java
+++ B/Point/src/PointClient.java
@@ -4,22 +4,41 @@ import java.awt.*;
 public class PointClient {
 
     public static void main(String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point(20, 100);
+        Point p1 = new Point(150, 150);
+        Point p2 = new Point(100, 87);
 
-        System.out.println("Point 1 is " + p1);
-        System.out.println("Point 2 is " + p2);
+        Point.earthquakeOrigin = new Point(160,160);
+        Point.earthquakeRadius = 23;
 
-        p2.translate(-3, 6);    // moves p2 left 3 and down 6
-        System.out.println("Point 2 is " + p2);
+        System.out.println(p1.getX() + ", " + p1.getY());
 
-        System.out.println("Point 2 is " + p2.distance(0,0) + " pixels from the origin (0,0)");
+        System.out.println("p1 is " + p1);
 
-        int sum = p2.x + p2.y;
-        System.out.println("Sum of coordinates of p2 = " + sum);
+        System.out.println(p2.getX() + ", " + p2.getY());
 
-        p1.x = 5;
-        System.out.println(p1);
+        DrawingPanel panel = new DrawingPanel(300, 300);
+        Graphics g = panel.getGraphics();
+
+        Point.drawEarthquakeImpactArea(g);
+        p1.draw(g);
+        p2.draw(g);
+
+//        Point p1 = new Point();
+//        Point p2 = new Point(20, 100);
+//
+//        System.out.println("Point 1 is " + p1);
+//        System.out.println("Point 2 is " + p2);
+//
+//        p2.translate(-3, 6);    // moves p2 left 3 and down 6
+//        System.out.println("Point 2 is " + p2);
+//
+//        System.out.println("Point 2 is " + p2.distance(0,0) + " pixels from the origin (0,0)");
+//
+//        int sum = p2.x + p2.y;
+//        System.out.println("Sum of coordinates of p2 = " + sum);
+//
+//        p1.x = 5;
+//        System.out.println(p1);
 
 //        DrawingPanel panel = new DrawingPanel(300, 300);
 //        Graphics g = panel.getGraphics();
